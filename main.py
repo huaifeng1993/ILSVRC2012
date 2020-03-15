c2f74f69cfc30594a4a65f75778a7d248f0c9a30
shenhuxiang <shhuixi@qq.com>
2020-03-15 10:41:33 +0800

    add

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ from trainer import Trainer
 from torch.nn import CrossEntropyLoss
 import torch.optim as optim
 from torch.optim import lr_scheduler
-from model.backbone import xceptionA
+from model.dfanet import XceptionA  
 from config import Config
 
 
@@ -19,20 +19,20 @@ if __name__=='__main__':
     val_dataset =  ILSVRC(ilsvrc_data_path='data',meta_path='data/meta.mat',val=True)
 
     train_loader = DataLoader(dataset=train_dataset,
-                                           batch_size=128, shuffle=True,
+                                           batch_size=256, shuffle=True,
                                            num_workers=8)
     val_loader = DataLoader(dataset=val_dataset,
-                                         batch_size=128, shuffle=False, 
+                                         batch_size=256, shuffle=False, 
                                          num_workers=8)
-    net = xceptionA(num_classes=1000)
+    net = XceptionA(cfg.CH_CFG[0],num_classes=1000)
     #load loss
     criterion = CrossEntropyLoss()
     optimizer = optim.SGD(
-    net.parameters(), lr=0.3, momentum=0.9,weight_decay=0.00004)  #select the optimizer
+    net.parameters(), lr=0.0001, momentum=0.9,weight_decay=0.00004)  #select the optimizer
 
     exp_lr_scheduler = lr_scheduler.StepLR(optimizer,30,gamma=0.1)
     trainer = Trainer('training', optimizer,exp_lr_scheduler, net, cfg, './log')
-    trainer.load_weights(trainer.find_last())
+    #trainer.load_weights(trainer.find_last())
     trainer.train(train_loader, val_loader, criterion, 60)
     #trainer.evaluate(valid_loader)
     print('Finished Training')
